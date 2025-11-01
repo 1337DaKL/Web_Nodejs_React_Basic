60b1ed598db9404d406baa4b5a26b9a51f87db38
Trịnh Đắc Lượng <luongtrinh2k3ndad@gmail.com>
2025-11-01 06:19:44 +0000

    update

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
-
-
 const app = express();
 app.use(cors());
 
-app.get("/:name" , (req , res) => {
-    const params  = req.params;
-    const name = params.name
-    res.send(`Hello ${name}`);
-})
+app.get("/:name", (req, res) => {
+  const params = req.params;
+  const name = params.name;
+  res.send(`Hello ${name}`);
+});
 
-app.listen(8080 , () => {
-    console.log("Da ket noi cong 8080");
-})
+app.listen(8080, () => {
+  console.log("Da ket noi cong 8080");
+});
